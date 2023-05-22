413efa486ba656dc3a7c33b1044635abb796b200
ArtP1 <123761472+ArtP1@users.noreply.github.com>
2023-05-22 10:25:40 +0000

    Fixed previous commit bugs. Added and Changed content
    # Fixed search functionalities of the Rent, For Sale, and Agents routes.
    
    # Added a Local Properties route.
    * This route allows users to view their local properties.

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ var zipcodes = require('zipcodes');
 var states = require('us-state-codes');
 const fetch = require('node-fetch');
 const app = express();
-var statesUrl = `https://csumb.space/api/allStatesAPI.php`;
 
 const options = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': 'bc7708f264msh880c9c36ec2445cp1e462ejsn03031156db1c',
+    'X-RapidAPI-Key': 'bd2b5a2586msh6687a14cd306d7ep1a744fjsn22255b88e536',
     'X-RapidAPI-Host': 'realty-in-us.p.rapidapi.com'
   }
-}; 
+};
 
-app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 // ------------------------------------ OTHER FUNCTIONS ------------------------------------
 async function fetchRealtyData(url) {
@@ -24,87 +23,16 @@ async function fetchRealtyData(url) {
     return data;
 }
 
-async function fetchStateData(url) {
-  let response = await fetch(url);
-  let data = await response.json();
 
-  return data;
-}
-
-async function shuffleJSON(url, jsonType) {  // Used this function to make sure that everything that is being displayed neglect being 'labeled' by the user's eye, if that makes sense.
-  let JSON = await fetchRealtyData(url)
-
-  /*
-    Credits to => The Fisher-Yates algorith
-    
-     for (let i = JSON.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          [JSON[i], JSON[j]] = [JSON[j], JSON[i]];
-        }
-        
-  */
-
-  switch(jsonType) {
-    case "sale":
-      
-        for (let i = JSON.properties.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          const temp = JSON.properties[i];
-          JSON.properties[i] = JSON.properties[j];
-          JSON.properties[j] = temp;
-        }
-
-      break;
-    case "rent":
-      
-        for (let i = JSON.listings.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          const temp = JSON.listings[i];
-          JSON.listings[i] = JSON.listings[j];
-          JSON.listings[j] = temp;
-        }
-
-      break;
-    case "agents":
-      
-        for (let i = JSON.agents.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          const temp = JSON.agents[i];
-          JSON.agents[i] = JSON.agents[j];
-          JSON.agents[j] = temp;
-        }
-
-      break;
-    default:
-      break;
-  }
-  
-
-  return JSON;
-}
 
 
 // HOME ------------------------------------------------------------------
-app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
+
+  
   console.log("Inside 'Home' route");
-  let ran = zipcodes.random();
-  
-  while(ran.country != "US") { // Reason for While: zipcodes npm package can either be zips from the US or Canada
-    ran = zipcodes.random();
-  }
 
-  // let url = `https://realty-in-us.p.rapidapi.com/properties/list-for-sale?state_code=${ran.state}&city=${ran.city}&offset=0&limit=5&sort=photos`;
-
-  // let locations = await fetchRealtyData(url);
-  
-  // while(locations.listings[0].photo === undefined || locations.listings === undefined) {
-  //   let locations = await fetchRealtyData(url);
-  // }
-
-  
-  // res.render('home',{"locationImg":locations.listings[0].photo});
-  // res.render('home',{"locationImg":locations.listings[0].photo, "locCity": ran.city});
-  res.render('home');
+  res.render('home')
 });
 
 
@@ -112,30 +40,29 @@ app.get('/', async (req, res) => {
 // RENT ------------------------------------------------------------------
 app.get('/rent', (req, res) => {
   console.log("Inside 'Rent' route");
-  res.render('rent',{"rent_listings": "", "length": ""});
+  res.render('rent',{"rent_listings": ""})
 });
 
 app.get('/rent-search', async (req, res) => {
   let state = req.query.state;
   let city = req.query.city;
   
-  console.log("State: " + state);
-  console.log("City: " + city);
-
   let url;  
   if(zipcodes.lookupByName(city, state).length > 0) {
     if(state.length == 2) {
-      url = `https://realty-in-us.p.rapidapi.com/properties/list-for-rent?state_code=${state}&city=${city}&limit=60&offset=0&sort=completeness&price_min=1750`;
+      url = `https://realty-in-us.p.rapidapi.com/properties/list-for-rent?state_code=${state}&city=${city}&limit=30&offset=0&sort=completeness&price_min=1750`;
     } else {
-      url = `https://realty-in-us.p.rapidapi.com/properties/list-for-rent?state_code=${states.getStateCodeByStateName(state)}&city=${city}&limit=60&offset=0&sort=completeness&price_min=1750`;
+      url = `https://realty-in-us.p.rapidapi.com/properties/list-for-rent?state_code=${states.getStateCodeByStateName(state)}&city=${city}&limit=30&offset=0&sort=completeness&price_min=1750`;
     }
     
-  let propertiesForRent = await shuffleJSON(url, "rent");
-    
+  let propertiesForRent = await fetchRealtyData(url);
+  console.log(propertiesForRent);
   if(propertiesForRent.listings.length > 0)
-    res.render('rent',{"rent_listings": propertiesForRent.listings, "length": (propertiesForRent.listings / 2) + 5});
+    res.render('rent', {"rent_listings": propertiesForRent.listings})
+    console.log("SUCCESS");
   } else {
-    res.render('rent',{"rent_listings": "", "length": ""});
+    res.render('rent', {"rent_listings": ""})
+    console.log("FAILED");
   }
 });
 
@@ -145,31 +72,30 @@ app.get('/rent-search', async (req, res) => {
 app.get('/forsale', (req, res) => {
   console.log("Inside 'For Sale' route");
   
-  res.render('forsale',{"for_sale_listings": "", "length": ""});
+  res.render('forsale',{"for_sale_listings": ""})
 });
 
 app.get('/forsale-search', async (req, res) => {
   let state = req.query.state;
   let city = req.query.city;
   
-  console.log("State: " + state);
-  console.log("City: " + city);
   
   let url;
   if(zipcodes.lookupByName(city, state).length > 0) {
     if(state.length == 2) {
-      url = `https://realty-in-us.p.rapidapi.com/properties/v2/list-for-sale?city=${city}&state_code=${state}&offset=0&limit=60&sort=freshest&price_min=400000`;
+      url = `https://realty-in-us.p.rapidapi.com/properties/v2/list-for-sale?city=${city}&state_code=${state}&offset=0&limit=30&sort=freshest&price_min=400000`;
     } else {
-      url = `https://realty-in-us.p.rapidapi.com/properties/v2/list-for-sale?city=${city}&state_code=${states.getStateCodeByStateName(state)}&offset=0&limit=60&sort=freshest&price_min=400000`;
+      url = `https://realty-in-us.p.rapidapi.com/properties/v2/list-for-sale?city=${city}&state_code=${states.getStateCodeByStateName(state)}&offset=0&limit=30&sort=freshest&price_min=400000`;
     }
       
-  let propertiesForSale = await shuffleJSON(url, "sale");
+  let propertiesForSale = await fetchRealtyData(url);
   
   if(propertiesForSale.properties.length > 0)
-    
-    res.render('forsale',{"for_sale_listings": propertiesForSale.properties, "length": (propertiesForSale.properties / 2) + 5});
+    res.render('forsale',{"for_sale_listings": propertiesForSale.properties})
+    console.log("SUCCESS");
   } else {
-    res.render('forsale',{"for_sale_listings": "", "length": ""});
+    res.render('forsale',{"for_sale_listings": ""})
+    console.log("FAILED");
   }
 });
 
@@ -177,11 +103,11 @@ app.get('/local-properties', async (req, res) => {
   console.log("Inside 'local-properties' route");
   let city = "Monterey";
   let state = "California";
-  let url = `https://realty-in-us.p.rapidapi.com/properties/v2/list-for-sale?city=${city}&state_code=${states.getStateCodeByStateName(state)}&offset=0&limit=60&sort=freshest&price_min=400000`;
+  let url = `https://realty-in-us.p.rapidapi.com/properties/v2/list-for-sale?city=${city}&state_code=${states.getStateCodeByStateName(state)}&offset=0&limit=30&sort=freshest&price_min=400000`;
 
-  let localPropForSaleShuffled = await shuffleJSON(url, "sale");
+  let localPropForSale = await fetchRealtyData(url);
   
-  res.render('localProperties')
+  res.render('localProperties', {"local_properties": localPropForSale.properties})
 });
 
 // FINANCE TOOLS  ------------------------------------------------------------------
@@ -189,7 +115,7 @@ app.get('/financetools', async (req, res) => {
   console.log("Inside 'Finance Tools' route");
 
   
-  res.render('financetools');
+  res.render('financetools')
 });
 
 
@@ -197,19 +123,11 @@ app.get('/financetools', async (req, res) => {
 
 
 // AGENTS  ------------------------------------------------------------------
-app.get('/agents', async (req, res) => {
+app.get('/agents',  (req, res) => {
   console.log("Inside 'Agents' route");
-  let randZipCode = zipcodes.random();
+
   
-  while(randZipCode.country != "US") { 
-    randZipCode = zipcodes.random();
-  }
-  
-  // let url = `https://realty-in-us.p.rapidapi.com/agents/list?postal_code=${randZipCode.zip}&offset=0&limit=9&sort=recent_activity_high&types=agent`;
-  // let data = await fetchRealtyData(url);
-  // console.log(data);
-  // res.render('agents', {"agentsList": data.agents, "agentCity": randZipCode.city});
-    res.render('agents');
+  res.render('agents', {"agentsList": "", "agentCity": ""})
 
 });
 
@@ -218,20 +136,25 @@ app.get('/agents-search', async (req, res) => {
   let zipcode = req.query.zipcode;
 
   let zipSearch = zipcodes.lookup(zipcode);
-
+  
   let url;
   if(zipSearch === undefined) {
-    url = `https://realty-in-us.p.rapidapi.com/agents/list?postal_code=${zipcode}&offset=0&limit=12&sort=recent_activity_high&types=agent`;  
+    res.render('agents', {"agentsList": "", "agentCity": ""})
+    console.log("FAILED");
+  } else {
+    url = `https://realty-in-us.p.rapidapi.com/agents/list?postal_code=${zipSearch.zip}&offset=0&limit=40&sort=recent_activity_high&types=agent`; 
   }
-  let data = await shuffleJSON(url, "agents");
+  let data = await fetchRealtyData(url);
 
   if(data.agents.length > 0) {
-     res.render('agents', {"agentsList": data.agents, "agentCity": randZipCode.city});
+     res.render('agents', {"agentsList": data.agents, "agentCity": ""})
+     console.log("SUCCESS");
   } else {
-     res.render('agents', {"agentsList": "", "agentCity": ""});
+    res.render('agents', {"agentsList": "", "agentCity": ""})
+    console.log("FAILED");
   }
 
-})
+});
 
 
 
--- public/js/script.js
+++ public/js/script.js
@@ -4,24 +4,57 @@ document.querySelector("#check_equ_rates").addEventListener("click", checkEquity
 document.querySelector("#finance_rates").addEventListener("click", financeRates);
 
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f307036bd8msh5a1cef3c7032b1cp174b46jsnb6c3361f3014',
+		'X-RapidAPI-Host': 'realty-in-us.p.rapidapi.com'
+	}
+};
 
 
-
-
+// async function fetchRealtyData(url) {
+//     let response = await fetch(url,options);
+//     let data = await response.json();
+  
+//     return data;
+// }
 
 /*
 URL = 'https://realty-in-us.p.rapidapi.com/mortgage/check-rates?creditScore=excellent&points=all&loanPurpose=purchase&loanTypes=ALL&loanPercent=85.2&propertyPrice=250000&zip=90230'
 */
-function checkRate() {
+async function checkRate() {
+  console.log("Checking Rates....");
   let creditScore = document.querySelector("#creditScoreCR").value;
   let points = document.querySelector("#pointsCR").value;
   let loanPurpose = document.querySelector("#loanPurposeCR").value;
-  let loanTypes = document.querySelector("#loanTypesCR").value;
+  let loanType = document.querySelector("#loanTypesCR").value;
   let loanPercent = document.querySelector("#loanPercentCR").value;
   let propertyPrice = document.querySelector("#propertyPriceCR").value;
   let zipcode = document.querySelector("#zipcodeCR").value;
 
-  console.log(creditScore + "\n" + points + "\n" + loanPurpose + "\n" + loanTypes + "\n" + loanPercent + "\n" + propertyPrice + "\n" + zipcode + "\n");
+
+  let url = `https://realty-in-us.p.rapidapi.com/mortgage/check-rates?creditScore=${creditScore}&points=${points}&loanPurpose=${loanPurpose}&loanTypes=${loanType}&loanPercent=${loanPercent}&propertyPrice=${propertyPrice}&zip=${zipcode}`;
+
+  let data = await fetch(url,options);
+  console.log(data);
+  
+  if(data.rates.length > 0) {
+    document.querySelector("#check_rate_results").innerHTML = `<div>
+                                                                <p>APR: ${data.rates[0].aprVal}%</p>
+                                                              </div>
+                                                              <div>
+                                                                <p>Loan Type: ${data.rates[0].loanTypeDisplayName}</p>
+                                                              </div>
+                                                              <div>
+                                                                <p>Monthly Payment: $${data.rates[0].loanPayment}</p>
+                                                              </div>
+                                                              <div>
+                                                                <p>Rate: ${data.rates[0].rate}%</p>
+                                                              </div>
+                                                            `; 
+  }
+   
 }
 
 
@@ -32,7 +65,8 @@ function checkRate() {
 /*
 URL = 'https://realty-in-us.p.rapidapi.com/mortgage/calculate-affordability?annual_income=250000&debt_to_income_ratio=0.43&down_payment=172000&homeowner_insurance_rate=0.012&interest_rate=0.03119&loan_term=30&monthly_debt=1500&tax_rate=0.01&hoa_fees=0&is_pmi_included=true'
 */
-function calcAffordability() {
+async function calcAffordability() {
+  console.log("Calculating Affordability....");
   let annualIncome = document.querySelector("#annualIncomeCA").value;
   let debt_to_income_ratio = document.querySelector("#debtToIncomeRationCA").value;
   let down_payment = document.querySelector("#downPaymentCA").value;
@@ -44,7 +78,12 @@ function calcAffordability() {
   let hoa_fees = document.querySelector("#hoaFeesCA").value;
   let is_pmi_included = document.querySelector("#isPmiInlcudedCA").value;
 
-  console.log(annualIncome + "\n" + debt_to_income_ratio + "\n" + down_payment + "\n" + homeowner_insurance_rate + "\n" + interest_rate + "\n" + loan_term + "\n" + monthly_debt + "\n" + tax_rate + "\n" + hoa_fees + "\n" + is_pmi_included + "\n");
+
+
+  let url = `https://realty-in-us.p.rapidapi.com/mortgage/calculate-affordability?annual_income=${annualIncome}&debt_to_income_ratio=${debt_to_income_ratio}&down_payment=${down_payment}&homeowner_insurance_rate=${homeowner_insurance_rate}&interest_rate=${interest_rate}&loan_term=${loan_term}&monthly_debt=${monthly_debt}&tax_rate=${tax_rate}&hoa_fees=${hoa_fees}&is_pmi_included=${is_pmi_included}`;
+
+  let data = await fetchRealtyData(url); 
+  console.log(data);
 }
 
 
@@ -57,6 +96,7 @@ function calcAffordability() {
 URL = 'https://realty-in-us.p.rapidapi.com/mortgage/check-equity-rates?creditScore=excellent&loanProduct=HELOAN_FIXED_20YEARS%2CHELOAN_FIXED_30YEARS&loanAmount=70000&propertyValue=300000&mortgageBalance=150000&zip=93505&state=CA'
 */
 function checkEquityRates() {
+  console.log("Checking Rates Affordability....");
   let creditScore = document.querySelector("#creditScoreCER").value;
   let loanProduct = document.querySelector("#loanProdCER").value;
   let loanAmount = document.querySelector("#loanAmountCER").value;
@@ -65,7 +105,6 @@ function checkEquityRates() {
   let zipcode = document.querySelector("#zipcodeCER").value;
 
 
-  console.log(creditScore + "\n" + loanProduct + "\n" + loanAmount + "\n" + propertyValue + "\n" + mortgageBalance + "\n" + zipcode + "\n");
 }
 
 
@@ -79,6 +118,7 @@ function checkEquityRates() {
 URL = 'https://realty-in-us.p.rapidapi.com/mortgage/check-rates?creditScore=excellent&points=all&loanPurpose=purchase&loanTypes=ALL&loanPercent=85.2&propertyPrice=250000&zip=90230'
 */
 function financeRates() {
+  console.log("Checking Financing Rates....");
   let creditScore = document.querySelector("#annualIncomeCA").value;
   let points = document.querySelector("#debtToIncomeRationCA").value;
   let loanPurpose = document.querySelector("#downPaymentCA").value;
